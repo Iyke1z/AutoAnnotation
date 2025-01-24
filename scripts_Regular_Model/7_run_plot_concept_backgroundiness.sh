d921c7b2b4e519480e72b5458c17ba3c892c995f
Iyke1z <157452358+Iyke1z@users.noreply.github.com>
2025-01-24 11:53:02 -0500

    Add files via upload

---
+++ scripts_Regular_Model/7_run_plot_concept_backgroundiness.sh
@@ -0,0 +1,4 @@
+
+python3 -m experiments.plot_concept_backgroundiness --model_name yolov5_nc7 --dataset_name coco2017 --labels "CRP Relevance,LRP,Activation"
+
+python3 -m experiments.plot_concept_backgroundiness --model_name yolov5_nc7 --dataset_name coco2017 --labels "CRP Relevance,Guided GradCAM,GradCAM,SSGradCAM"
