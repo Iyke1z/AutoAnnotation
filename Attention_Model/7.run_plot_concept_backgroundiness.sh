505ea9ee0c85e8d02003cd6fe45d13239fb284c4
Iyke1z <157452358+Iyke1z@users.noreply.github.com>
2025-01-24 11:47:46 -0500

    Delete Attention_Model directory

--- Attention_Model/7.run_plot_concept_backgroundiness.sh
+++
@@ -1,4 +0,0 @@
-
-python3 -m experiments.plot_concept_backgroundiness --model_name yolov5_nc7_attention --dataset_name coco2017 --labels "CRP Relevance,LRP,Activation"
-
-python3 -m experiments.plot_concept_backgroundiness --model_name yolov5_nc7_attention --dataset_name coco2017 --labels "CRP Relevance,Guided GradCAM,GradCAM,SSGradCAM"
