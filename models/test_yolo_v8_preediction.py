6a5134c80b0cd2f180c0491192a48775a2ae9aaa
Iyke1z <157452358+Iyke1z@users.noreply.github.com>
2025-01-24 11:24:55 -0500

    Delete models directory

--- models/test_yolo_v8_preediction.py
+++
@@ -1,4 +0,0 @@
-from ultralytics import YOLO
-
-yolo8_model_detection=YOLO('/home/projects/xai/exaplaiable_ai/runs/detect/train55/weights/best.pt')
-yolo8_model_detection.predict(source='/home/projects/xai/exaplaiable_ai/data/', save=True )
